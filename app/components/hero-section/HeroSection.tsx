09d6471a914347163956261dba03719aaa081a9c
Weverton <wevertonbezzera013@gmail.com>
2023-08-04 01:00:46 -0300

    fix: hero section css

--- app/components/hero-section/HeroSection.tsx
+++ app/components/hero-section/HeroSection.tsx
@@ -66,35 +66,26 @@ export default function HeroSection() {
                         <div className="container">
                             <HeroSectionProfileImage />
                         </div>
-                        <div className="md:hidden pt-10 flex flex-col justify-between ">
-                            <a
-                                className="pl-10"
-                                href="https://github.com/wevertonbezzera013"
-                            >
+                        <div className="md:hidden pt-10 flex flex-col justify-between">
+                            <a href="https://github.com/wevertonbezzera013">
                                 <Icon
                                     name="FaGithub"
                                     className="text-white bg-primary rounded-full w-9 h-9 p-2"
                                 />
                             </a>
-                            <a className="pl-10" href="wa.me/5521968179138">
+                            <a href="wa.me/5521968179138">
                                 <Icon
                                     name="FaWhatsapp"
                                     className="text-white bg-primary rounded-full w-9 h-9 p-2"
                                 />
                             </a>
-                            <a
-                                className="pl-10"
-                                href="https://www.linkedin.com/in/weverton-bezerra-da-costa-061ab11a4/"
-                            >
+                            <a href="https://www.linkedin.com/in/weverton-bezerra-da-costa-061ab11a4/">
                                 <Icon
                                     name="FaLinkedin"
                                     className="text-white bg-primary rounded-full w-9 h-9 p-2"
                                 />
                             </a>
-                            <a
-                                className="pl-10"
-                                href="mailto:wevertonbezzera013@gmail.com"
-                            >
+                            <a href="mailto:wevertonbezzera013@gmail.com">
                                 <Icon
                                     name="MdMail"
                                     className="text-white bg-primary rounded-full w-9 h-9 p-2"
