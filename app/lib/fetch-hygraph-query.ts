6a12ddc42f1ca761a5433e13f4df22ae0b24cf7c
Weverton <wevertonbezzera013@gmail.com>
2023-08-15 17:18:49 -0300

    fix: fetch api

--- app/lib/fetch-hygraph-query.ts
+++ app/lib/fetch-hygraph-query.ts
@@ -1,16 +1,10 @@
-export const fetchHygraphQuery = async <T>(
-    query: string,
-    revalidate?: number
-): Promise<T> => {
+export const fetchHygraphQuery = async <T>(query: string): Promise<T> => {
     const response = await fetch(process.env.HYGRAPH_URL!, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
             Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
-        },
-        next: {
-            revalidate,
         },
         body: JSON.stringify({
             query,
