ddba1cd531493b3e8cb94b6e22739768d2e4f221
Weverton <wevertonbezzera013@gmail.com>
2023-08-31 20:44:14 -0300

    fix: added dynamic metadata

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,6 +2,14 @@ import { getProjects } from "../../../sanity/sanity-utils";
 import { ProjectIntroductionSection } from "../../components/introduction-section/project-Introduction-section/ProjectIntroductionSection";
 import { ProjectPage } from "../../components/projects-section/ProjectPage";
 
+export const metadata = async () => {
+    const projects = await getProjects();
+    return {
+        title: projects[0].name,
+        description: projects[0].description,
+    };
+};
+
 export default async function Project() {
     const projects = await getProjects();
     return (
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,10 @@ import { getProjects } from "../../sanity/sanity-utils";
 import { IntroductionSection } from "../components/introduction-section/IntroductionSection";
 import { ProjectsSectionGrid } from "../components/projects-section/project-page/ProjectsSectionGrid";
 
+export const metadata = {
+    title: "Projects",
+};
+
 export default async function Projects() {
     const projects = await getProjects();
     return (
