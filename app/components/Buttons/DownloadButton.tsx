057d2fc64846104635a1eabeca7b9422c45b2b7e
Weverton <wevertonbezzera013@gmail.com>
2023-08-31 17:06:23 -0300

    tweak: small changes

--- app/components/Buttons/DownloadButton.tsx
+++ app/components/Buttons/DownloadButton.tsx
@@ -1,11 +1,16 @@
 export default function DownloadButton() {
     return (
         <>
-            <a className="sm:hidden md:block" href="weverton's cv frontend developer.pdf">
+            <a
+                className="sm:hidden md:block"
+                href="weverton's cv frontend developer.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <button className="text-white bg-primary hover:bg-secondary ease-in-out duration-300 pl-7 pr-7 p-3 rounded-sm">
                     Download CV
                 </button>
             </a>
         </>
-    )
-};
+    );
+}
