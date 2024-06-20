7549ab118bffef72a444be40cb394c3807b3aaf6
Weverton <wevertonbezzera013@gmail.com>
2024-06-20 15:59:46 -0300

    fix: testimonial issues

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
                 </div>
             ))}
             <ProjectsSection projects={projects} />
-            <TestimonialsSection />
+            {/* <TestimonialsSection /> */}
             <WorkExperienceSection />
         </>
     );
